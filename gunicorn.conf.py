cf1175f234563e47f1870df5f7cfbb8b3d3d2cec
João Barbosa Silva <iotabe@gmail.com>
2025-07-28 13:08:58 -0300

    porta docker

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 import os
 
 # Configurações do servidor
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "eventlet"
 worker_connections = 1000
